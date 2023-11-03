73c1f26d3b9b6fa8c196c4775c2f10fa5fba6951
Andrey <andreytomson84@gmail.com>
2023-11-03 17:05:37 +0200

    add Error Boundaries and Pagination

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,9 @@
+import img from './error-gojo.gif';
+
+const ErrorMessage = () => {
+    return (
+        <img style={{ display:'block' }} src={img} alt="Error"/>
+    )
+}
+
+export default ErrorMessage;
