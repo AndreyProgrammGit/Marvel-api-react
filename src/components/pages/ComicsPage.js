a15364cb19a0e3ee4b97399e2b947088c2c9cb8d
Andrey <andreytomson84@gmail.com>
2023-12-06 20:00:52 +0200

    React Router v5

---
+++ src/components/pages/ComicsPage.js
@@ -0,0 +1,15 @@
+import AppBaner from "../appBanner/AppBanner"
+import ComicsList from "../comicsList/ComicsList";
+
+const ComicsPage = () => {
+
+
+    return (
+        <>
+            <AppBaner/>
+            <ComicsList/>
+        </>
+    )
+}
+
+export default ComicsPage;
